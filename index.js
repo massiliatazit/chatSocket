78b94fef806d2334b1153d3ffa3d2c6c234f0636
massiliatazit <massiliatazit@users.noreply.github.com>
2021-02-10 21:31:57 +0100

    server created

---
+++ index.js
@@ -0,0 +1,20 @@
+const router =require('express')()
+const server = require ("http").Server(router)
+const io = require('socket.io')(server);
+
+
+const port =5666
+server.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+})
+
+router.get('/',(req,res)=>{
+    res.sendFile(__dirname + "./public/index.html")
+})
+io.on('connection',(socket)=>{
+    console.log('user connected')
+    socket.emit('message',{manny:"hey how's going there"})// call the event 
+    socket.on('another event',(data)=>{
+        console.log(data)
+    })
+})
